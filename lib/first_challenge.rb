f3fed48af566c08dc316004df029290ab1743ac1
Cherissa Woudenberg <snerzel@comcast.net>
2021-04-08 17:35:18 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -31,7 +31,27 @@ def first_challenge
          :name => "Lord Capulet",
          :age => "50"
        },
-       :matriarch =>
+       :matriarch => {
+         :name => "Lady Capulet",
+         :age => "51"
+       },
+       :heroine => {
+         :name => "Juliet",
+         :age => "15",
+         :status => "alive"
+       },
+       :hero_friends => [
+         {
+           :name => "Steven",
+           :age => "30",
+           :attitude => "confused"
+         },
+         {
+           :name => "Nurse",
+           :age => "44",
+           :attitude => "worried"
+         }
+       ]
      }
    }
 end
